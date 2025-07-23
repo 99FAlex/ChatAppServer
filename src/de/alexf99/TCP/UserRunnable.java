c45c0c577fe4d43f2bc22ff871982384445f19ce
99FAlex <127439399+99FAlex@users.noreply.github.com>
2025-07-23 17:35:25 +0200

    Username

--- src/de/alexf99/TCP/UserRunnable.java
+++ src/de/alexf99/TCP/UserRunnable.java
@@ -8,6 +8,7 @@ import java.util.Dictionary;
 public class UserRunnable implements Runnable{
 
     private Socket clientSocket;
+    private String username;
     UserRunnable(Socket clientSocket){
         this.clientSocket = clientSocket;
     }
@@ -24,18 +25,43 @@ public class UserRunnable implements Runnable{
             while ((bytesRead = in.read(buffer)) != -1) {
                 String receivedData = new String(buffer, 0, bytesRead, StandardCharsets.UTF_8);
                 System.out.println(Thread.currentThread().getName() + " || " +receivedData);
-                TcpServer.clients.forEach((name, socket) -> {
-                    try {
-                        socket.getOutputStream().write(receivedData.getBytes(StandardCharsets.UTF_8));
-                        socket.getOutputStream().flush();
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                });
-            }
-        } catch (Exception e) {
-            System.out.println("Test");
-            System.out.println(e.getMessage());        }
+                if (username == null){
+                    username = receivedData;
+                }else {
+                    TcpServer.clients.forEach((name, socket) -> {
+                        try {
+                            System.out.println(name);
+                            String message = username + " --> " + receivedData;
+                            socket.getOutputStream().write(message.getBytes(StandardCharsets.UTF_8));
+                            socket.getOutputStream().flush();
+                        } catch (IOException e) {
+                            System.out.println(e.getMessage());
+                        }
+                    });
+                }
 
+            }
+        }catch(IOException e){
+            System.out.println("IOExeption for client " + Thread.currentThread().getName() + ": " + e.getMessage());
+        }catch (Exception e){
+            System.out.println("General Exception for client " + Thread.currentThread().getName() + ": " + e.getMessage());
+        }finally {
+            try {
+                if (clientSocket != null && !clientSocket.isClosed()){
+                    clientSocket.close();
+                    System.out.println("Socket closed || " + Thread.currentThread().getName());
+                }
+
+            } catch (IOException e) {
+                System.out.println("Error closing Connection : " + e.getMessage());
+            }
+            try {
+                TcpServer.clients.remove(Thread.currentThread().getName());
+                Thread.currentThread().join();
+            } catch (InterruptedException e) {
+                System.out.println("Error thread joining : " + e.getMessage());
+            }
+
+        }
     }
 }
