2f028920717fb44486bbfe2bf049cc0d05c6775e
99FAlex <127439399+99FAlex@users.noreply.github.com>
2025-08-19 20:19:19 +0200

    Commands

--- src/de/alexf99/Main.java
+++ src/de/alexf99/Main.java
@@ -5,7 +5,8 @@ import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Scanner;
-
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
 
 
 public class Main {
@@ -15,6 +16,15 @@ public class Main {
         DataInputStream in = new DataInputStream(System.in);
         File messageFile = new File("messages.txt");
         messageFile.createNewFile();
+
+        /*Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try (PrintWriter out = new PrintWriter(new FileWriter("shutdown_log.txt", true))) {
+                out.println("The program is shutting down at " + new java.util.Date());
+                out.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }));*/
 
         int port = 8001;
 
@@ -54,11 +64,68 @@ public class Main {
 
         System.out.println("Server starting on port " + port);
 
+        CompletableFuture<Void> future = readConsoleAsync();
+
         tcpServer.start(port);
+
+
     }
 
-    public static void addMessage(String msg) throws IOException {
+    public static void changePort(String port) throws IOException {
+        File config = new File("config.txt");
+        Scanner configReader = new Scanner(config);
+        String portline = configReader.nextLine();
+        configReader.close();
+        FileWriter configWriter = new FileWriter("config.txt");
+
+        String portConf = portline.split("=")[1];
 
 
+        configWriter.write("port=" + port);
+        configWriter.close();
+
+    }
+
+    private static final java.util.concurrent.ExecutorService executor = Executors.newCachedThreadPool();
+
+
+
+    public static CompletableFuture<Void> readConsoleAsync() {
+        return CompletableFuture.runAsync(() -> {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                    while (true){
+                        System.out.println("Bitte gib etwas ein:");
+                        String line = reader.readLine();
+                        if (line != null && line.equalsIgnoreCase("port")) {
+                            System.out.println("Enter new port");
+                            String port = reader.readLine();
+                            changePort(port);
+                            System.out.println("Please restart the server");
+                            System.out.println("Stopping...");
+                            Thread.sleep(1000);
+                            System.exit(0);
+
+                        }else if (line != null && line.equalsIgnoreCase("stop")){
+                            System.exit(0);
+                        }else if(line != null && line.equalsIgnoreCase("help")){
+                            System.out.println(Colors.text_BRIGHT_BLUE + "List of Commands");
+                            System.out.println(Colors.text_BRIGHT_BLUE + "port - to change the port");
+                            System.out.println(Colors.text_BRIGHT_BLUE + "stop - stops the server");
+                            System.out.println(Colors.text_BRIGHT_BLUE + "help - shows this menu" + Colors.text_RESET);
+
+                        }else {
+                            System.out.println(Colors.text_RED + "Command not found. Please user " + Colors.text_BRIGHT_BLUE + "help" + Colors.text_RED + " to see every command" + Colors.text_RESET);
+                        }
+                    }
+
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+
+
+        }, executor);
     }
 }
--- src/de/alexf99/TCP/TcpServer.java
+++ src/de/alexf99/TCP/TcpServer.java
@@ -1,10 +1,12 @@
 package de.alexf99.TCP;
 
 import de.alexf99.Colors;
+import de.alexf99.Main;
 
 import java.io.IOException;
 import java.net.*;
 import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 
 public class TcpServer {
